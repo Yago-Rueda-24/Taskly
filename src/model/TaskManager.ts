14cccd0f22a79d1f34a77dd8e659456c6a08baac
Yago <yago.rc.04@gmail.com>
2025-02-06 09:22:03 +0100

    Creación de la clase contenedora TaskManager
    Se encargara de gestionar las operaciones CRUD que se hagan en la
    aplicación para comunicarse con la UI

---
+++ src/model/TaskManager.ts
@@ -0,0 +1,22 @@
+import Task from "./Task";
+
+export default class TaskManager{
+
+    private TaskList :Array<Task>;
+
+    public constructor(){
+
+        this.TaskList = new Array();
+    }
+
+    public addTask(aux:Task){
+        this.TaskList.push(aux);
+    }
+
+    public removeTask(aux:Task){
+
+        this.TaskList = this.TaskList.filter(item=> item !== aux);
+        
+    }
+
+}
