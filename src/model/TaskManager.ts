6435615faa363a4fb2ebd689b332d44bbc7e6781
yago-vasco <yago.rc.04@gmail.com>
2025-02-11 22:09:36 +0100

    Corrección de errores e implementaciñon de nuevos metodos para cubriir funcionaliades necesarias

--- src/model/TaskManager.ts
+++ src/model/TaskManager.ts
@@ -10,6 +10,8 @@ export default class TaskManager {
         this.TaskList = new Array();
     }
 
+
+
     public addTask(taskName: string, taskContent: string): number {
         const auxtask: Task = new Task(taskName, taskContent);
 
@@ -21,14 +23,35 @@ export default class TaskManager {
         return 0;
     }
 
-    public removeTask(removedTaskName: string) {
+    public removeTask(removedTaskName: string):number {
+
+        if (!this.Tasklist.some(element => element.taskName == removedTaskName)) {
+            return 1;
+        }
 
         this.TaskList = this.TaskList.filter(item => item.taskName != removedTaskName);
+        return 0;
+
+    }
+
+    public modifyName(oldName:string,newName:string):number{
+
+
+        let findIndex = this.TaskList.findIndex(element=> element.taskName == oldName);
+        if(findIndex === -1){
+            return 1;
+        }
+        this.TaskList.at(findIndex)!.taskName = newName;
+        return 0;
 
     }
 
     public get Tasklist(): Array<Task> {
         return this.TaskList;
+    }
+
+    public get length(): number {
+        return this.TaskList.length;
     }
 
     public toJSON() {
