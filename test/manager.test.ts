0d0f0b6ae236fcee242366a572e4c53f7d1eb441
yago-vasco <yago.rc.04@gmail.com>
2025-02-11 22:08:50 +0100

    Implementación de los nuevos Tests del manager

--- test/manager.test.ts
+++ test/manager.test.ts
@@ -3,14 +3,26 @@ import TaskManager from "../src/model/TaskManager.js";
 
 let manager = new TaskManager();
 
-test("Añadir una entrada a la lista",()=>{
-    expect(manager.addTask("hola","mundo")).toBe(0);
+test("Añadir una entrada a la lista", () => {
+    expect(manager.addTask("hola", "mundo")).toBe(0);
 });
-test("Eliminar un entrada de las lista",()=>{
+test("Eliminar un entrada de las lista", () => {
     manager.removeTask("hola")
     expect(manager.Tasklist.length).toBe(0);
 });
-test("Añadir una entrada repetida a la lista",()=>{
-    manager.addTask("hola","mundo")
-    expect(manager.addTask("hola","mundo")).toBe(1);
+test("Eliminar una entrada inexistente", () => {
+    expect(manager.removeTask("hola")).toBe(1);
+});
+test("Añadir una entrada repetida a la lista", () => {
+    manager.addTask("hola", "mundo");
+    expect(manager.addTask("hola", "mundo")).toBe(1);
+});
+
+test("Longitud de la lista", () => {
+    expect(manager.length).toBe(1);
+});
+test("modificación del nombre de una tarea", () => {
+    
+    expect(manager.modifyName("hola","adios")).toBe(0);
+    expect(manager.modifyName("hola","adios")).toBe(1);
 });
